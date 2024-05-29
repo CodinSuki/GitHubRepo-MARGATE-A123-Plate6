162074a7146ef23b5a30b9f4fa4ef84544e46e26
Sky <122413784+CodinSuki@users.noreply.github.com>
2024-05-30 06:54:00 +0800

    Update ProblemNumberTwo.java

--- ProblemNumberTwo.java
+++ ProblemNumberTwo.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class ProblemNumberTwo {
     int MargateVertices;
-    int matrix[][];
+    int[][] MargateMatrix;
     
-    ProblemNumberTwo(int MargateVertices){ // Constructor; vertices as argument
-        this.vertices = MargateVertices; // vertices = number of nodes in the graph
-        matrix = new int[vertices][MargateVertices]; // allocate memory
+    ProblemNumberTwo(int MargateVertices) { // Constructor; vertices as argument
+        this.MargateVertices = MargateVertices; // vertices = number of nodes in the graph
+        MargateMatrix = new int[MargateVertices][MargateVertices]; // allocate memory
     }
     
-    void inputMatrix(Scanner scanner) { // Method to input the adjacency matrix
-        System.out.println("Enter the adjacency matrix:"); //since vertices = nodes therefore nodes = number of rows and columns in the graph
-        for (int i = 0; i < vertices; i++) { // so in this code we input matrix depending on the number of vertices
-            for (int j = 0; j < vertices; j++) { // so the loop goes on depending on the number we input say 3 will = 3 x 3 
-                matrix[i][j] = scanner.nextInt();  //stoores it in matrix [i][j] // number 	1 represents an edge
+    void MargateInputMatrix(Scanner scanner) { // Method to input the adjacency matrix
+        System.out.println("Enter the adjacency matrix:");
+        for (int i = 0; i < MargateVertices; i++) {
+            for (int j = 0; j < MargateVertices; j++) {
+                MargateMatrix[i][j] = scanner.nextInt();
             }
         }
     }
     
-    void listEdges() {// counts the times each edge appears 
+    void MargateListEdges() { // counts the times each edge appears
         System.out.println("Edges and their counts:");
-        for (int i = 0; i < vertices; i++) { 
-            for (int j = 0; j < vertices; j++) {
-                if (matrix[i][j] > 0) { //checks if the numberr in matrix i and j is greater than 0, therefore it has an edge and prints that number
-                    System.out.println("Edge from " + i + " to " + j + " appears " + matrix[i][j] + " times");
+        for (int i = 0; i < MargateVertices; i++) {
+            for (int j = 0; j < MargateVertices; j++) {
+                if (MargateMatrix[i][j] > 0) { // checks if the number in MargateMatrix[i][j] is greater than 0
+                    System.out.println("Edge from " + i + " to " + j + " appears " + MargateMatrix[i][j] + " times");
                 }
             }
         }
@@ -33,12 +33,12 @@ public class ProblemNumberTwo {
     
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter number of Vertices: ");
-            int vertices = scanner.nextInt();
-            ProblemNumberTwo graph = new ProblemNumberTwo(vertices);
+            System.out.print("Enter number of MargateVertices: ");
+            int MargateVertices = scanner.nextInt();
+            ProblemNumberTwo graph = new ProblemNumberTwo(MargateVertices);
             
-            graph.inputMatrix(scanner); //inputting the matrix of the scannerr 
-            graph.listEdges(); //listing the edges
+            graph.MargateInputMatrix(scanner); // inputting the matrix using the scanner
+            graph.MargateListEdges(); // listing the edges
         }
     }
 }
