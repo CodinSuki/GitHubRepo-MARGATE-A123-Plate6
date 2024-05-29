668c15679a2f2cae22efef3b530f468c04790752
Sky <122413784+CodinSuki@users.noreply.github.com>
2024-05-29 19:56:23 +0800

    Update PrblemNumberSix.java

--- PrblemNumberSix.java
+++ PrblemNumberSix.java
@@ -1,13 +1,13 @@
 package PLATE6;
 import java.util.Scanner;
 
-public class PrblemNumberSix {
-    int vertices;
+public class MargatePrblemNumberSix {
+    int Margatevertices;
     int matrix[][];
     //initializes the mtrix	
-    PrblemNumberSix(int vertices){ // Constructor; vertices as argument
-        this.vertices = vertices; // vertices = number of nodes in the graph
-        matrix = new int[vertices][vertices]; // allocate memory, adjacency matrix is a square matrix dimension to the number of nodes e.g 3 x 3 = 3 vertices
+    MargatePrblemNumberSix(int Margatevertices){ // Constructor; vertices as argument
+        this.Margatevertices = Margatevertices; // vertices = number of nodes in the graph
+        matrix = new int[Margatevertices][Margatevertices]; // allocate memory, adjacency matrix is a square matrix dimension to the number of nodes e.g 3 x 3 = 3 Margatevertices
     }
     
     void addEdge(int source, int destination) { // Allow multiple edges and loops
@@ -15,8 +15,8 @@ public class PrblemNumberSix {
     }
     
     void printGraph() {
-        for (int i = 0; i < vertices; i++) { //row oone of the graph
-        	for (int j = 0; j < vertices; j++) { //foollowing column of the graph
+        for (int i = 0; i < Margatevertices; i++) { //row oone of the graph
+        	for (int j = 0; j < Margatevertices; j++) { //foollowing column of the graph
                 System.out.print(matrix[i][j] + " "); //prints out matrix[i][j] which is equal to 0 unless it is 1 then it will increment by 1	 
             } 
             System.out.println();
@@ -25,13 +25,13 @@ public class PrblemNumberSix {
     
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter number of Vertices: ");
-            int vertices = scanner.nextInt();
-            PrblemNumberSix graph = new PrblemNumberSix(vertices); //connects the vertices input to prroblem number six constructor so that it knows
-            System.out.print("Enter number of Edges: ");		//how many nodes are in the graph
-            int edges = scanner.nextInt();
+            System.out.print("Enter number of vertices: ");
+            int MargateMargatevertices = scanner.nextInt();
+            MargatePrblemNumberSix graph = new MargatePrblemNumberSix(Margatevertices); //connects the vertices input to prroblem number six constructor so that it knows
+            System.out.print("Enter number of Margateedges: ");		//how many nodes are in the graph
+            int Margateedges = scanner.nextInt();
             
-            for (int i = 0; i < edges; i++) { //code to looop edges question based on edges input
+            for (int i = 0; i < Margateedges; i++) { //code to looop edges question based on edges input
                 System.out.println("Enter the source vertex: ");
                 int source = scanner.nextInt();
                 System.out.println("Enter the destination vertex: ");
