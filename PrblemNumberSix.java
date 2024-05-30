97de46a234a6b65db9eecad5a28742c77b49c8b9
Sky <122413784+CodinSuki@users.noreply.github.com>
2024-05-30 22:00:17 +0800

    Update PrblemNumberSix.java

--- PrblemNumberSix.java
+++ PrblemNumberSix.java
@@ -1,23 +1,24 @@
 package PLATE6;
 import java.util.Scanner;
 
-public class MargatePrblemNumberSix {
-    int Margatevertices;
-    int matrix[][];
-    //initializes the mtrix	
-    MargatePrblemNumberSix(int Margatevertices){ // Constructor; vertices as argument
-        this.Margatevertices = Margatevertices; // vertices = number of nodes in the graph
-        matrix = new int[Margatevertices][Margatevertices]; // allocate memory, adjacency matrix is a square matrix dimension to the number of nodes e.g 3 x 3 = 3 Margatevertices
+public class ProblemNumberSix {
+    int MargateVertices;
+    int MargateMatrix[][];
+    
+    // Initializes the matrix
+    ProblemNumberSix(int MargateVertices) { // Constructor; vertices as argument
+        this.MargateVertices = MargateVertices; // vertices = number of nodes in the graph
+        MargateMatrix = new int[MargateVertices][MargateVertices]; // allocate memory, adjacency matrix is a square matrix dimension to the number of nodes e.g 3 x 3 = 3 vertices
     }
     
-    void addEdge(int source, int destination) { // Allow multiple edges and loops
-        matrix[source][destination]++; //if the link is present then the number is added/incremented 
+    void MargateAddEdge(int MargateSource, int MargateDestination) { // Allow multiple edges and loops
+        MargateMatrix[MargateSource][MargateDestination]++; // if the link is present then the number is added/incremented 
     }
     
-    void printGraph() {
-        for (int i = 0; i < Margatevertices; i++) { //row oone of the graph
-        	for (int j = 0; j < Margatevertices; j++) { //foollowing column of the graph
-                System.out.print(matrix[i][j] + " "); //prints out matrix[i][j] which is equal to 0 unless it is 1 then it will increment by 1	 
+    void MargatePrintGraph() {
+        for (int i = 0; i < MargateVertices; i++) { // row one of the graph
+            for (int j = 0; j < MargateVertices; j++) { // following column of the graph
+                System.out.print(MargateMatrix[i][j] + " "); // prints out MargateMatrix[i][j] which is equal to 0 unless it is 1 then it will increment by 1	 
             } 
             System.out.println();
         }
@@ -25,30 +26,27 @@ public class MargatePrblemNumberSix {
     
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter number of vertices: ");
-            int MargateMargatevertices = scanner.nextInt();
-            MargatePrblemNumberSix graph = new MargatePrblemNumberSix(Margatevertices); //connects the vertices input to prroblem number six constructor so that it knows
-            System.out.print("Enter number of Margateedges: ");		//how many nodes are in the graph
-            int Margateedges = scanner.nextInt();
+            System.out.print("Enter number of MargateVertices: ");
+            int MargateVertices = scanner.nextInt();
+            ProblemNumberSix MargateGraph = new ProblemNumberSix(MargateVertices); // connects the vertices input to ProblemNumberSix constructor so that it knows how many nodes are in the graph
             
-            for (int i = 0; i < Margateedges; i++) { //code to looop edges question based on edges input
-                System.out.println("Enter the source vertex: ");
-                int source = scanner.nextInt();
-                System.out.println("Enter the destination vertex: ");
-                int destination = scanner.nextInt();
+            System.out.print("Enter number of MargateEdges: ");
+            int MargateEdges = scanner.nextInt();
+            
+            for (int i = 0; i < MargateEdges; i++) { // code to loop edges question based on edges input
+                System.out.println("Enter the MargateSource vertex: ");
+                int MargateSource = scanner.nextInt();
+                System.out.println("Enter the MargateDestination vertex: ");
+                int MargateDestination = scanner.nextInt();
                 
-                graph.addEdge(source, destination); //calls upon .addEdge for the source and destination 
+                MargateGraph.MargateAddEdge(MargateSource, MargateDestination); // calls upon .MargateAddEdge for the source and destination 
             }
             
-            graph.printGraph();
+            MargateGraph.MargatePrintGraph();
             
-        } catch(Exception e) { // catch method made to catch error if source/destination is overr the vertices given
-        	System.out.println("Please enter the proper source/destination: ");
-        	e.printStackTrace();
-        	
+        } catch (Exception e) { // catch method made to catch error if source/destination is over the vertices given
+            System.out.println("Please enter the proper source/destination: ");
+            e.printStackTrace();
         }
-    
-     
     }
-   
 }
