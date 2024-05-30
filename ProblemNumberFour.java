6bfe44e058f77c62e4e15ce8e89bf0287d900199
Sky <122413784+CodinSuki@users.noreply.github.com>
2024-05-30 09:28:42 +0800

    Update ProblemNumberFour.java

--- ProblemNumberFour.java
+++ ProblemNumberFour.java
@@ -1,56 +1,55 @@
 package PLATE6;
+
 import java.util.Scanner;
 
 public class ProblemNumberFour {
-    int vertices;
-    int matrix[][];
+    int MargateVertices;
+    int[][] MargateMatrix;
     
     // Initializes the matrix
-    ProblemNumberFour(int vertices){  // Constructor; vertices as argument
-        this.vertices = vertices; // vertices = number of nodes in the graph
-        matrix = new int[vertices][vertices]; // Allocate memory for the adjacency matrix
+    ProblemNumberFour(int MargateVertices) {  // Constructor; vertices as argument
+        this.MargateVertices = MargateVertices; // vertices = number of nodes in the graph
+        MargateMatrix = new int[MargateVertices][MargateVertices]; // Allocate memory for the adjacency matrix
     }
     
     // Adds an edge to the undirected graph
-    void addEdge(int source, int destination) {  // Constructor; int source and int destination as arguments
-        matrix[source][destination]++; 
-        matrix[destination][source]++; //for undirected graph aadd the edge in both directions
+    void MargateAddEdge(int MargateSource, int MargateDestination) {  // Method to add an edge
+        MargateMatrix[MargateSource][MargateDestination]++;
+        MargateMatrix[MargateDestination][MargateSource]++; // For undirected graph add the edge in both directions
     }
     
     // Calculates and prints the degree of each vertex
-    void printDegrees() {
-        for (int i = 0; i < vertices; i++) {
-            int degree = 0; //initialize degreee as 0 in the for loop so that it also iterates with the
-            for (int j = 0; j < vertices; j++) { // loop so that its value is kept after it has been added
-                degree += matrix[i][j]; // Sum of row elements gives the degree of the vertex
+    void MargatePrintDegrees() {
+        for (int i = 0; i < MargateVertices; i++) {
+            int MargateDegree = 0; // Initialize degree as 0
+            for (int j = 0; j < MargateVertices; j++) {
+                MargateDegree += MargateMatrix[i][j]; // Sum of row elements gives the degree of the vertex
             }
-            System.out.println("Degree of vertex " + i + " is: " + degree);
+            System.out.println("Degree of vertex " + i + " is: " + MargateDegree);
         }
     }
     
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter number of Vertices: ");
-            int vertices = scanner.nextInt();
-            ProblemNumberFour graph = new ProblemNumberFour(vertices); 
+            Scanner scanner = new Scanner(System.in) {
+            System.out.print("Enter number of MargateVertices: ");
+            int MargateVertices = scanner.nextInt();
+            ProblemNumberFour graph = new ProblemNumberFour(MargateVertices); 
             
-            System.out.print("Enter number of Edges: ");
-            int edges = scanner.nextInt();
+            System.out.print("Enter number of MargateEdges: ");
+            int MargateEdges = scanner.nextInt();
             
-            for (int i = 0; i < edges; i++) {
-                System.out.println("Ent	er the source vertex: ");
-                int source = scanner.nextInt();
-                System.out.println("Enter the destination vertex: ");
-                int destination = scanner.nextInt();
+            for (int i = 0; i < MargateEdges; i++) {
+                System.out.print("Enter the MargateSource vertex: ");
+                int MargateSource = scanner.nextInt();
+                System.out.print("Enter the MargateDestination vertex: ");
+                int MargateDestination = scanner.nextInt();
                 
-                graph.addEdge(source, destination); 
+                graph.MargateAddEdge(MargateSource, MargateDestination); 
             }
            
-            graph.printDegrees(); 
+            graph.MargatePrintDegrees(); 
             
-        } catch(Exception e) {
-            System.out.println("Please enter the proper source/destination: ");
-            e.printStackTrace();
+       
         }
     }
 }
